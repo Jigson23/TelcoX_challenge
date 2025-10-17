21b628a5d69697307f3c01bb89e0041b032e8d97
Jigson Contreras <33557994+Jigson23@users.noreply.github.com>
2025-10-17 14:59:55 -0500

    chore: drop binary favicon asset

---
+++ frontend/telcox-dashboard/src/app/app.config.ts
@@ -0,0 +1,18 @@
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideRouter } from '@angular/router';
+import { MessageService } from 'primeng/api';
+
+import { routes } from './app.routes';
+import { errorInterceptor } from './core/interceptors/error.interceptor';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideAnimations(),
+    provideHttpClient(withInterceptors([errorInterceptor])),
+    MessageService
+  ]
+};
